6b3234cd3193cc30085b2500a9586091e5c5d4d2
marwen <marwanbk2000@gmail.com>
2024-01-26 10:29:40 +0100

    Update version and URLs in pubspec.yaml

--- android/src/main/java/flutter/lock/task/flutter_lock_task/FlutterLockTaskPlugin.java
+++ android/src/main/java/flutter/lock/task/flutter_lock_task/FlutterLockTaskPlugin.java
@@ -1,6 +1,8 @@
 package flutter.lock.task.flutter_lock_task;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import android.app.Activity;
 import android.app.ActivityManager;
@@ -8,8 +10,8 @@ import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.provider.Settings;
 import android.os.Build;
+import android.provider.Settings;
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -21,144 +23,148 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry;
 
-/** FlutterLockTaskPlugin */
 public class FlutterLockTaskPlugin implements FlutterPlugin, ActivityAware, MethodCallHandler, PluginRegistry.ActivityResultListener {
 
-  private MethodChannel channel;
-  private Context appContext;
-  private Activity mActivity;
-  DevicePolicyManager dpm;
-  ComponentName cn;
+    private MethodChannel channel;
+    private Context appContext;
+    private Activity mActivity;
+    private DevicePolicyManager dpm;
+    private ComponentName cn;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    appContext = flutterPluginBinding.getApplicationContext();
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_lock_task");
-    channel.setMethodCallHandler(this);
-  }
+    private static final List<String> WHITELISTED_PACKAGES = Arrays.asList(
+    "com.android.settings" // For general settings
+    );
 
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    cn = new ComponentName(appContext, DeviceAdmin.class);
-    dpm = (DevicePolicyManager) appContext.getSystemService(Context.DEVICE_POLICY_SERVICE);
-
-    if (call.method.equals("startLockTask")) {
-      try {
-        if (mActivity != null) {
-          if (dpm.isDeviceOwnerApp(mActivity.getPackageName())) {
-            ArrayList<String> packages = new ArrayList<>();
-            packages.add(mActivity.getPackageName());
-        
-            dpm.setLockTaskPackages(cn, packages.toArray(new String[0]));
-            mActivity.startLockTask();
-            result.success(true);
-          } else {
-            mActivity.startLockTask();
-            result.success(true);
-          }
-        } else {
-          result.success(false);
-        }
-      } catch (Exception e) {
-        result.success(false);
-      }
-    } else if (call.method.equals("stopLockTask")) {
-      try {
-        if (mActivity != null) {
-          mActivity.stopLockTask();
-          result.success(true);
-        } else {
-          result.success(false);
-        }
-      } catch (Exception e) {
-        result.success(false);
-      }
-    } else if (call.method.equals("isInLockTaskMode")) {
-      try {
-        if (mActivity != null) {
-          ActivityManager activityManager = (ActivityManager) mActivity.getSystemService(Context.ACTIVITY_SERVICE);
-
-          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            int lockTaskMode = activityManager.getLockTaskModeState();
-            result.success(lockTaskMode != ActivityManager.LOCK_TASK_MODE_NONE);
-          } else {
-            result.success(activityManager.isInLockTaskMode());
-          }
-        } else {
-          result.success(false);
-        }
-      } catch (Exception e) {
-        result.success(false);
-      }
-    } else if (call.method.equals("setDeviceOwnerApp")) {
-      try {
-        if (mActivity != null) {
-          Runtime.getRuntime().exec(new String[]{"su","-c", "dpm set-device-owner " + mActivity.getPackageName() + "/flutter.lock.task.flutter_lock_task.DeviceAdmin"});
-          result.success(true);
-        } else {
-          result.success(false);
-        }
-      } catch (Exception e) {
-        result.success(false);
-      }
-    } else if (call.method.equals("clearDeviceOwnerApp")) {
-      try {
-        if (mActivity != null) {
-          dpm.clearDeviceOwnerApp(mActivity.getPackageName());
-          result.success(true);
-        } else {
-          result.success(false);
-        }
-      } catch (Exception e) {
-        result.success(false);
-      }
-    } else if (call.method.equals("openHomeSettings")) {
-      try {
-        if (mActivity != null) {
-          final Intent intent = new Intent(Settings.ACTION_HOME_SETTINGS);
-          mActivity.startActivity(intent);
-          result.success(true);
-        } else {
-          result.success(false);
-        }
-      } catch (Exception e) {
-        result.success(false);
-      }
-    } else if (call.method.equals("getPackageName")) {
-      result.success(mActivity.getPackageName());
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        appContext = flutterPluginBinding.getApplicationContext();
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "flutter_lock_task");
+        channel.setMethodCallHandler(this);
+        cn = new ComponentName(appContext, DeviceAdmin.class);
+        dpm = (DevicePolicyManager) appContext.getSystemService(Context.DEVICE_POLICY_SERVICE);
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-      channel.setMethodCallHandler(null);
-  }
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        switch (call.method) {
+            case "startLockTask":
+                startLockTask(result);
+                break;
+            case "stopLockTask":
+                stopLockTask(result);
+                break;
+            case "isInLockTaskMode":
+                isInLockTaskMode(result);
+                break;
+            case "openHomeSettings":
+                openHomeSettings(result);
+                break;
+            case "getPackageName":
+                getPackageName(result);
+                break;
+            default:
+                result.notImplemented();
+        }
+    }
 
-  @Override
-  public void onAttachedToActivity(@NonNull ActivityPluginBinding activityPluginBinding) {
-    this.mActivity = activityPluginBinding.getActivity();
-    activityPluginBinding.addActivityResultListener(this);
-}
+    private void startLockTask(@NonNull Result result) {
+        try {
+            if (mActivity != null && dpm.isDeviceOwnerApp(mActivity.getPackageName())) {
+                ArrayList<String> packages = new ArrayList<>(WHITELISTED_PACKAGES);
+                packages.add(mActivity.getPackageName());
+                dpm.setLockTaskPackages(cn, packages.toArray(new String[0]));
+                mActivity.startLockTask();
+                result.success(true);
+            } else {
+                result.success(false);
+            }
+        } catch (Exception e) {
+            result.error("LOCK_TASK_ERROR", "Failed to start lock task: " + e.getMessage(), null);
+        }
+    }
 
-  @Override
-  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding activityPluginBinding) {
-    onAttachedToActivity(activityPluginBinding);
-  }
+    private void stopLockTask(@NonNull Result result) {
+        try {
+            if (mActivity != null) {
+                mActivity.stopLockTask();
+                result.success(true);
+            } else {
+                result.success(false);
+            }
+        } catch (Exception e) {
+            result.error("LOCK_TASK_ERROR", "Failed to stop lock task: " + e.getMessage(), null);
+        }
+    }
 
-  @Override
-  public void onDetachedFromActivityForConfigChanges() {
-    this.mActivity = null;
-  }
+    private void isInLockTaskMode(@NonNull Result result) {
+        try {
+            if (mActivity != null) {
+                ActivityManager activityManager = (ActivityManager) mActivity.getSystemService(Context.ACTIVITY_SERVICE);
+                boolean inLockTaskMode;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    inLockTaskMode = activityManager.getLockTaskModeState() != ActivityManager.LOCK_TASK_MODE_NONE;
+                } else {
+                    inLockTaskMode = activityManager.isInLockTaskMode();
+                }
+                result.success(inLockTaskMode);
+            } else {
+                result.success(false);
+            }
+        } catch (Exception e) {
+            result.error("LOCK_TASK_ERROR", "Failed to check lock task mode: " + e.getMessage(), null);
+        }
+    }
 
-  @Override
-  public void onDetachedFromActivity() {
-    this.mActivity = null;
-  }
+    private void openHomeSettings(@NonNull Result result) {
+        try {
+            if (mActivity != null) {
+                final Intent intent = new Intent(Settings.ACTION_HOME_SETTINGS);
+                mActivity.startActivity(intent);
+                result.success(true);
+            } else {
+                result.success(false);
+            }
+        } catch (Exception e) {
+            result.error("LOCK_TASK_ERROR", "Failed to open home settings: " + e.getMessage(), null);
+        }
+    }
 
-  @Override
-  public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-    return false;
-  }
+    private void getPackageName(@NonNull Result result) {
+        if (mActivity != null) {
+            result.success(mActivity.getPackageName());
+        } else {
+            result.success(null);
+        }
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding activityPluginBinding) {
+        this.mActivity = activityPluginBinding.getActivity();
+        activityPluginBinding.addActivityResultListener(this);
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding activityPluginBinding) {
+        this.mActivity = activityPluginBinding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        this.mActivity = null;
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        this.mActivity = null;
+    }
+
+    @Override
+    public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+        return false;
+    }
 }
--- test/flutter_lock_task_test.dart
+++ test/flutter_lock_task_test.dart
@@ -7,13 +7,49 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 class MockFlutterLockTaskPlatform
     with MockPlatformInterfaceMixin
     implements FlutterLockTaskPlatform {
-
   @override
   Future<String?> getPackageName() => Future.value('');
+
+  @override
+  Future<bool> clearDeviceOwnerApp() {
+    // TODO: implement clearDeviceOwnerApp
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> isInLockTaskMode() {
+    // TODO: implement isInLockTaskMode
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> openHomeSettings() {
+    // TODO: implement openHomeSettings
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> setDeviceOwnerApp() {
+    // TODO: implement setDeviceOwnerApp
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> startLockTask() {
+    // TODO: implement startLockTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> stopLockTask() {
+    // TODO: implement stopLockTask
+    throw UnimplementedError();
+  }
 }
 
 void main() {
-  final FlutterLockTaskPlatform initialPlatform = FlutterLockTaskPlatform.instance;
+  final FlutterLockTaskPlatform initialPlatform =
+      FlutterLockTaskPlatform.instance;
 
   test('$MethodChannelFlutterLockTask is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelFlutterLockTask>());
